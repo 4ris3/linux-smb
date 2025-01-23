bbe5a55cd0944968e7fdc5bc6a21019e1f57ca36
Dawid - MacOS <dawidstachowiczzz@gmail.com>
2025-01-23 17:31:04 +0100

    change permission to working others with nogroup

--- linux-smb.sh
+++ linux-smb.sh
@@ -6,9 +6,9 @@ cd smb/
 sudo mkdir admin_files
 sudo mkdir others_files
 sudo chown $user:$user admin_files
-sudo chmod 770 admin_files
+sudo chmod 2770 admin_files
 sudo chown nobody:nogroup others_files
-sudo chmod 777 others_files
+sudo chmod 2777 others_files
 #download and start samba
 sudo apt install samba -y
 sudo systemctl start smbd
@@ -17,4 +17,4 @@ sudo rm /etc/samba/smb.conf
 sudo mv /home/$user/server/smb.conf /etc/samba/
 sudo sed -i "s/changeitquickly/$user/g" /etc/samba/smb.conf
 echo -e "1234\n1234" | sudo smbpasswd -a $user
-sudo systemctl restart smb
+sudo systemctl restart smbd
