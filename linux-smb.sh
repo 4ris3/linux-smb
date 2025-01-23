95879010ec3fc5ee48f9b67e143fbdfe7d7b6397
Dawid - MacOS <dawidstachowiczzz@gmail.com>
2025-01-23 16:17:13 +0100

    Del test file add functions smb.conf and better perm and ownerships

--- linux-smb.sh
+++ linux-smb.sh
@@ -3,9 +3,32 @@ cd /srv/
 sudo mkdir smb
 sudo mkdir admin_files
 sudo mkdir others_files
-sudo chmod 700 admin_files
+sudo group add admin
+sudo chown admin:admin admin_files
+sudo chmod 770 admin_files
 sudo chown nobody:nogroup others_files
 sudo chmod 777 others_files
 #download and start samba
 sudo apt install samba -y
 sudo systemctl start smbd
+#config smb.conf
+sudo tee -a /etc/samba/smb.conf << EOF
+
+[global]
+map to guest = Bad User
+guest account = nobody
+
+[others]
+path = /srv/smb/others_files
+browseable = yes
+writable = yes
+guest ok = yes
+read only = no
+
+[admin]
+path = /srv/smb/admin_files
+browseable = yes
+writable = yes
+valid users = admin
+read only = no
+EOF
--- linux-test.sh
+++
@@ -1,5 +0,0 @@
-sudo tee -a /etc/samba/smb.conf << EOF
-[global]
-map to guest = Bad User
-guest account = nobody
-EOF
