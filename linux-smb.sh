7006c1156e3f2bf6d53c7e23c207ac737402a1b4
dawid <dawidstachowiczzz@gmail.com>
2025-01-23 14:24:34 +0000

    Add install and run samba section

--- linux-smb.sh
+++ linux-smb.sh
@@ -1,3 +1,4 @@
+#create folders admin_files and others_files and set permissions/own
 cd /srv/
 sudo mkdir smb
 sudo mkdir admin_files
@@ -5,3 +6,6 @@ sudo mkdir others_files
 sudo chmod 700 admin_files
 sudo chown nobody:nogroup others_files
 sudo chmod 777 others_files
+#download and start samba
+sudo apt install samba -y
+sudo systemctl start smbd
